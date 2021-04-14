3075702982edde993ebd7785f5481032f7f56b23
techguy <dev.techguy@gmail.com>
2021-04-14 15:09:35 +0300

    :construction: Work in progress.

---
+++ src/sample/DashBoardController.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashBoardController implements Initializable {
+    @FXML
+    private ImageView dashImage;
+    @FXML
+    private MenuItem exitApp;
+
+    public void exitMenuItemAction(ActionEvent event) {
+        exitApp.setOnAction((ActionEvent t) -> {
+            System.exit(0);
+        });
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+        File bannerFile = new File("images/dashboard.png");
+        Image bannerImage = new Image(bannerFile.toURI().toString());
+        dashImage.setImage(bannerImage);
+    }
+}
--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -2,23 +2,32 @@ package sample;
 
 import com.sun.javafx.scene.ImageViewHelper;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.stage.StageStyle;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LoginController implements Initializable {
 
@@ -58,13 +67,34 @@ public class LoginController implements Initializable {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         Connection connectDB = databaseConnection.getConnection();
 
-        String slq_query = "SELECT username,password FROM users WHERE username ='" + usernameTextField.getText() + "' && password = '" + passwordTextField.getText() + "' ";
+        String sqlQuery = "SELECT username,password FROM users WHERE username ='" + usernameTextField.getText() + "' && password = '" + passwordTextField.getText() + "' ";
 
         try {
-            ResultSet resultSet = connectDB.createStatement().executeQuery(slq_query);
-
+            // database variables
+            Statement statement = connectDB.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
             if (resultSet.next()) {
-                JOptionPane.showMessageDialog(null, "Welcome...", "System Notification", JOptionPane.INFORMATION_MESSAGE);
+                try {
+                    Stage stageClose = (Stage) loginButton.getScene().getWindow();
+                    stageClose.close();
+
+
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("dashboard.fxml"));
+                    /*
+                     * if "fx:controller" is not set in fxml
+                     * fxmlLoader.setController(NewWindowController);
+                     */
+                    Scene scene = new Scene(fxmlLoader.load(), 630, 400);
+                    Stage stage = new Stage();
+                    stage.setTitle("DashBoard");
+                    stage.initStyle(StageStyle.UNDECORATED);
+                    stage.setScene(scene);
+                    stage.show();
+                } catch (IOException e) {
+                    Logger logger = Logger.getLogger(getClass().getName());
+                    logger.log(Level.SEVERE, "Failed to create new Window.", e);
+                }
             } else {
                 JOptionPane.showMessageDialog(null, "Wrong credentials", "System Notification", JOptionPane.WARNING_MESSAGE);
                 passwordTextField.setText("");
